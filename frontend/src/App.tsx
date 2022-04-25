06e9942b1d42ca7bb9398ede221352a1ced544b6
Edwin Kato <katedwin88@gmail.com>
2022-04-25 12:50:31 +0300

    chore: add style libraries, precommit and linting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,12 @@
-import {
-  ChakraProvider,
-  Box,
-  Grid,
-  theme,
-} from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
-import { Videos } from "./components/Videos"
+import { ChakraProvider, Box, Grid, Text, theme } from '@chakra-ui/react'
+import { ColorModeSwitcher } from './ColorModeSwitcher'
+import { Videos } from './components/Videos'
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Box textAlign="center" fontSize="xl">
       <Grid p={6}>
+        <Text>Videos</Text>
         <ColorModeSwitcher justifySelf="flex-end" />
         <Videos />
       </Grid>
--- frontend/src/components/Videos.tsx
+++ frontend/src/components/Videos.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from 'react'
 import {
   ChakraProvider,
   Box,
@@ -6,52 +6,52 @@ import {
   Grid,
   GridItem,
   theme,
-} from "@chakra-ui/react"
+} from '@chakra-ui/react'
 
 interface Video {
-    attributes: {
-        slug: string;
-        title: string;
-        url: string;
-        isPublic: boolean;
-    }
-    id: number;
+  attributes: {
+    slug: string
+    title: string
+    url: string
+    isPublic: boolean
+  }
+  id: number
 }
 
 export const Videos = () => {
-    const [videos, setVideos] = useState<Video[]>([]);
+  const [videos, setVideos] = useState<Video[]>([])
 
-    useEffect(() => {
-        const url = "http://localhost:1337/api/videos";
+  useEffect(() => {
+    const url = 'http://localhost:1337/api/videos'
 
-        const fetchData = async () => {
-            try {
-                const response = await fetch(url);
-                const json = await response.json();
-                console.log(json);
-                setVideos(json.data);
-            } catch (error) {
-                console.log("error", error);
-            }
-        };
-
-        fetchData();
-    }, []);
-
-    return (
-  <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-        <Grid templateColumns='repeat(5, 1fr)' gap={6}>
-          {videos.map(({ id, attributes: { title, url } }) => (
-                <GridItem key={id}>
-                    <Text>{title}</Text>
-                    <video controls>
-                        <source src={url} />
-                    </video>
-                </GridItem>
-          ))}
-      </Grid>
-    </Box>
-  </ChakraProvider>
-)
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const json = await response.json()
+        console.log(json)
+        setVideos(json.data)
+      } catch (error) {
+        console.log('error', error)
+      }
     }
+
+    fetchData()
+  }, [])
+
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid templateColumns="repeat(5, 1fr)" gap={6}>
+          {videos.map(({ id, attributes: { title, url } }) => (
+            <GridItem key={id}>
+              <Text>{title}</Text>
+              <video controls>
+                <source src={url} />
+              </video>
+            </GridItem>
+          ))}
+        </Grid>
+      </Box>
+    </ChakraProvider>
+  )
+}
